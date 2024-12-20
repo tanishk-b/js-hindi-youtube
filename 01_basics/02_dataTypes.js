1489b2f3f073afdc2e6e5e7b8a9fb93f5b7a85f6
tanishk-b <tanishkb05@gmail.com>
2024-12-20 03:29:34 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log (3 
+    + 
+      3); // code readability should be high 
+
+
+console.log("Tanishk")
+
+let name = "tanishk"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bignit
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefind => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefind
